2d93e0c8e15ab60a94d680fa7728c9b110282604
William <mrwwhitney@gmail.com>
2019-01-19 14:38:23 -0600

    --

--- src/main/java/JavaSystemMonitor/Constants.java
+++ src/main/java/JavaSystemMonitor/Constants.java
@@ -5,9 +5,11 @@ public class Constants
 
     public static final int UPDATE_RATE_MS = 1000;
 
+    //Convert factor
     public static final long BYTES_TO_GIGABYTES = 1024 * 1024 * 1024;
 
     public static final int DEFAULT_WIDTH = 500;
 
     public static final int DEFAULT_HEIGHT = 500;
+
 }
