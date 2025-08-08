a6bef766c2be54b40d815a46bb5c63e9f77fa2a5
William <mrwwhitney@gmail.com>
2025-08-08 13:17:01 -0500

    Show memory usage units

--- src/main/java/JavaSystemMonitor/Constants.java
+++ src/main/java/JavaSystemMonitor/Constants.java
@@ -5,6 +5,8 @@ public class Constants
 
     public static final int UPDATE_RATE_MS = 1000;
 
+    public static final long BYTES_TO_MEGABYTES = 1024 * 1024;
+
     public static final long BYTES_TO_GIGABYTES = 1024 * 1024 * 1024;
 
     public static final int DEFAULT_WIDTH = 500;
