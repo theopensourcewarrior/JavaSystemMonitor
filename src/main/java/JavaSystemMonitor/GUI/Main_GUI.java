f97a245c3b4a4f6b0a2ee60c92c01df07b6e52f1
William Whitney <mrwwhitney@gmail.com>
2025-08-08 13:39:51 -0500

    Changed to all in one view

--- src/main/java/JavaSystemMonitor/GUI/Main_GUI.java
+++ src/main/java/JavaSystemMonitor/GUI/Main_GUI.java
@@ -1,5 +1,8 @@
 package JavaSystemMonitor.GUI;
 
+import JavaSystemMonitor.GUI.Disk.DiskUsage;
+import java.awt.GridLayout;
+
 public class Main_GUI extends javax.swing.JFrame
 {
 
@@ -9,6 +12,12 @@ public class Main_GUI extends javax.swing.JFrame
     public Main_GUI()
     {
         initComponents();
+
+        setLayout(new GridLayout(2, 2));
+        add(new CPU_Usage());
+        add(new MemoryUsage());
+        add(new NetworkUsage());
+        add(new DiskUsage());
 
     }
 
@@ -20,39 +29,23 @@ public class Main_GUI extends javax.swing.JFrame
     private void initComponents()
     {
 
-        javax.swing.JTabbedPane tabs = new javax.swing.JTabbedPane();
-        CPU_UsagePanel = new JavaSystemMonitor.GUI.CPU_Usage();
-        memoryUsagePanel = new JavaSystemMonitor.GUI.MemoryUsage();
-        diskUsagePanel = new JavaSystemMonitor.GUI.Disk.DiskUsage();
-        networkUsage1 = new JavaSystemMonitor.GUI.NetworkUsage();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Java System Monitor");
-
-        tabs.addTab("CPU Usage", CPU_UsagePanel);
-        tabs.addTab("Memory Usage", memoryUsagePanel);
-        tabs.addTab("Disk Usage", diskUsagePanel);
-        tabs.addTab("Network Usage", networkUsage1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(tabs, javax.swing.GroupLayout.DEFAULT_SIZE, 475, Short.MAX_VALUE)
+            .addGap(0, 780, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(tabs, javax.swing.GroupLayout.DEFAULT_SIZE, 291, Short.MAX_VALUE)
+            .addGap(0, 504, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private JavaSystemMonitor.GUI.CPU_Usage CPU_UsagePanel;
-    private JavaSystemMonitor.GUI.Disk.DiskUsage diskUsagePanel;
-    private JavaSystemMonitor.GUI.MemoryUsage memoryUsagePanel;
-    private JavaSystemMonitor.GUI.NetworkUsage networkUsage1;
     // End of variables declaration//GEN-END:variables
-
 }
